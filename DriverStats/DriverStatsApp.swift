dd289dc35b3244731e32e1458b76cc7a64d2d9e5
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2026-06-07 22:28:21 -0700

    Initial Commit

---
+++ DriverStats/DriverStatsApp.swift
@@ -0,0 +1,32 @@
+//
+//  DriverStatsApp.swift
+//  DriverStats
+//
+//  Created by Pierce Oxley on 7/6/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DriverStatsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DriverStatsTests/DriverStatsTests.swift
@@ -0,0 +1,19 @@
+//
+//  DriverStatsTests.swift
+//  DriverStatsTests
+//
+//  Created by Pierce Oxley on 7/6/26.
+//
+
+import Testing
+@testable import DriverStats
+
+struct DriverStatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
